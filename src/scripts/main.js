643f1b5f069158ac54ee9d265e748ec3397288fb
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-19 16:09:44 -0500

    First click, setup. Second click: setup with punchline. Third click: new joke setup. Button text isn't changing. Working on that next

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,29 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-// import jokeData from '../api/jokeData';
+import jokeData from '../api/jokeData';
 
 const init = () => {
+  let btnText = 'Get A Joke';
+
   document.querySelector('#app').innerHTML = `<div>
-  <h1>Setup</h1>
-  <h4>Punchline</h4>
-  <button type="button" id="jokeBtn" class="btn btn-outline-dark">Get A Joke</button>
+  <h1 id='setup'></h1>
+  <h4 id='delivery'></h4>
+  <button type="button" id="jokeBtn" class="btn btn-outline-dark">${btnText}</button>
   </div>
   `;
+
+  document.querySelector('#jokeBtn').addEventListener('click', () => {
+    jokeData().then((response) => {
+      if (btnText === 'Get A Joke' || btnText === 'Get Another Joke') {
+        document.querySelector('#setup').innerHTML = response.setup;
+        document.querySelector('#delivery').innerHTML = '';
+        btnText = 'Get A Punchline';
+      } else if (btnText === 'Get A Punchline') {
+        document.querySelector('#delivery').innerHTML = response.delivery;
+        btnText = 'Get Another Joke';
+      }
+    });
+  });
 };
 
 init();
